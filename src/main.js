2eadb320663bd66bf43ca5a27fb046903039f143
Andrew <ratnikov.am@gmail.com>
2025-06-09 20:46:33 +0300

    Add order buttons and event handlers for mentorship and consultation in index.html and main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 import "./style.css";
+
+const ordersHandler = {
+  init() {
+    this.eventHandlers();
+  },
+
+  eventHandlers() {
+    const mentorshipOrderButton = document.querySelector(
+      "#mentorship-order-btn"
+    );
+    mentorshipOrderButton.addEventListener("click", this.mentorshipOrder);
+
+    const consultationOrderButton = document.querySelector(
+      "#consultation-order-btn"
+    );
+    consultationOrderButton.addEventListener("click", this.consultationOrder);
+  },
+
+  mentorshipOrder() {
+    console.log("Mentorship order clicked");
+  },
+
+  consultationOrder() {
+    console.log("Consultation order clicked");
+  },
+};
+
+ordersHandler.init();
