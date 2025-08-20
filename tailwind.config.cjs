bbb2bf535689f9666c030d841170e71ba7e0f8c5
Andrew <ratnikov.am@gmail.com>
2025-08-20 21:52:53 +0300

    fix: Apply styles and fix final migration issues
    - Add global styles and Tailwind CSS to the layout
    - Configure Tailwind to use custom theme colors
    - Correct a typo in the `rounded-4xl` class to `rounded-3xl`
    - Move Firebase and JavaScript logic to the correct components
    - Remove old, unused files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        'primary-light': 'var(--color-primary-light)',
+        'primary-dark': 'var(--color-primary-dark)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+      },
+    },
   },
   plugins: [],
 }
